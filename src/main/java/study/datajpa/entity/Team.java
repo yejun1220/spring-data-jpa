0b3692e69d37337969aeb8760c7822da321ec9ea
yejun_1220@naver.com <yejun_1220@naver.com>
2022-07-05 12:00:25 +0900

    도메인 모델

---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,27 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter @Setter
+@ToString(of = {"id", "teamName"})
+@NoArgsConstructor(access = PROTECTED)
+public class Team {
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String teamName;
+
+    public Team(String teamName) {
+        this.teamName = teamName;
+    }
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+}
