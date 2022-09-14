c89f2133db2e162f6a1e1687108b1ce119801e0d
im-yejun <yejun_1220@naver.com>
2022-09-14 17:18:07 +0900

    쿼리 메소드 기능 - @EntityGraph

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -20,6 +20,10 @@ import static lombok.AccessLevel.PROTECTED;
         name = "Member.findByUsername",
         query = "select m from Member m where m.username = :username"
 )
+@NamedEntityGraph(
+        name = "Member.all",
+        attributeNodes = @NamedAttributeNode("team")
+)
 public class Member {
 
     @Id
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -276,4 +276,30 @@ public class MemberRepositoryTest {
         assertThat(resultCount).isEqualTo(4);
         assertThat(findMember.getAge()).isEqualTo(21);
     }
+
+    @Test
+    public void findMemberLazy() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> all = memberRepository.findEntityGraphByUsername("member1");
+        for (Member member : all) {
+            System.out.println("member = " + member);
+            System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
+            System.out.println("member.getTeam().getTeamName() = " + member.getTeam().getTeamName());
+        }
+
+    }
 }
